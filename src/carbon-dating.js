33b109e6052ed3ffc66d8c8a3b051d6663eaa519
HooliganGS <gaga.borovikov.ilia12@gmail.com>
2021-02-24 09:08:26 +0300

    feat:complite task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const CONSTNUMBER = 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+
+    if (typeof(+sampleActivity) != Number) {
+        return false;
+    }
+    let activityRate = MODERN_ACTIVITY / sampleActivity;
+    let a = CONSTNUMBER / HALF_LIFE_PERIOD;
+    let result = Math.ceil((Math.log(activityRate)) / a);
+    return result;
 };
